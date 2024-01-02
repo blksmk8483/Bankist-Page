b9cac70584050f1fbaac0eaf914e20e4d138190e
Brandon Knight <knightbrandon1984@gmail.com>
2024-01-02 00:14:01 -0600

    added forEach for the modal and some notes for myself

--- script.js
+++ script.js
@@ -2,28 +2,30 @@
 
 ///////////////////////////////////////
 // Modal window
-
 const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.btn--close-modal');
 const btnsOpenModal = document.querySelectorAll('.btn--show-modal');
 
-const openModal = function () {
-  modal.classList.remove('hidden');
-  overlay.classList.remove('hidden');
+// OPENS MY MODAL - this shows the modal and the overlay
+const openModal = function (e) {
+  e.preventDefault();
+  modal.classList.remove('hidden'); // html line 335
+  overlay.classList.remove('hidden'); // html line 351
 };
 
+btnsOpenModal.forEach(btn => btn.addEventListener('click', openModal));
+
+// CLOSES MY MODAL - this hides it when I click on it
 const closeModal = function () {
-  modal.classList.add('hidden');
-  overlay.classList.add('hidden');
+  modal.classList.add('hidden'); // html line 335
+  overlay.classList.add('hidden'); // html line 351
 };
-
-for (let i = 0; i < btnsOpenModal.length; i++)
-  btnsOpenModal[i].addEventListener('click', openModal);
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
 
+// This closes the modal if I click outside of the box
 document.addEventListener('keydown', function (e) {
   if (e.key === 'Escape' && !modal.classList.contains('hidden')) {
     closeModal();
