efc86f7bdea7bfd09b79a9b920e85eaff7524aba
Brandon Knight <knightbrandon1984@gmail.com>
2024-01-06 20:02:59 -0600

    revealing elements on Scroll

--- script.js
+++ script.js
@@ -130,27 +130,6 @@ nav.addEventListener('mouseout', function (e) {
 // --------   STICKY NAVIGATION   --------
 ///////////////////////////////////////
 
-// const intialCoords = section1.getBoundingClientRect();
-
-// window.addEventListener('scroll', function (e) {
-//   if (window.scrollY > intialCoords.top) nav.classList.add('sticky');
-//   else nav.classList.remove('sticky');
-// });
-
-// Sticky navigation: Intersection Observer API
-// const obsCallback = function (entries, observer) {
-//   entries.forEach(entry => {
-//     console.log(entry);
-//   });
-// };
-// const obsOptions = {
-//   root: null,
-//   threshold: [0, 0.2],
-// };
-
-// const observer = new IntersectionObserver(obsCallback, obsOptions);
-// observer.observe(section1);
-
 const header = document.querySelector('.header');
 const navHeight = nav.getBoundingClientRect().height;
 console.log(navHeight);
@@ -163,13 +142,39 @@ const stickyNav = function (entries) {
   else nav.classList.remove('sticky');
 };
 
-// the rootMargin is the same heigh as the navbar
+// the rootMargin is the same height as the navbar
 const headerObserver = new IntersectionObserver(stickyNav, {
   root: null,
   threshold: 0,
   rootMargin: `-${navHeight}px`,
 });
 headerObserver.observe(header);
+
+///////////////////////////////////////
+// --------   REVEALING ELEMENTS ON SCROLL   --------
+///////////////////////////////////////
+
+// Revealing sections
+const allSections = document.querySelectorAll('.section');
+
+const revealSection = function (entries, observer) {
+  const [entry] = entries;
+  console.log(entry);
+
+  if (!entry.isIntersecting) return;
+  entry.target.classList.remove('section--hidden');
+  observer.unobserve('entry-target');
+};
+
+const sectionObserver = new IntersectionObserver(revealSection, {
+  root: null,
+  threshold: 0.15,
+});
+
+allSections.forEach(function (section) {
+  sectionObserver.observe(section);
+  section.classList.add('section--hidden');
+});
 
 // --------   PRACTICE HAS NOTHING TO DO WITH THE PROJECT   --------
 
